5a873464fffae1b181dfbb549a14f06f2b7a433f
Rajatsinha05 <rajatsinha5467@gmail.com>
2023-01-30 22:51:00 +0530

    frontend

--- src/Pages/Posts.jsx
+++ src/Pages/Posts.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React  , {useState,useEffect}from 'react';
 import Navbar from '../Components/Navbar';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
 function Home() {
+
+    const [data , setData] = useState([]);
+    const cookies = new Cookies();
+    const jwt = cookies.get('jwt')
+
+    async function getData(){
+
+            axios.post('' , {
+                token:jwt
+            }).then((res)=>{
+                
+            }).catch((Err)=>{
+                console.log(Err);
+            })
+
+    }
+    
+    useEffect(() => {
+      return () => {
+      
+      };
+    }, [])
+
     return (
         <>
             <Navbar />
+
 
             <div className="outer_home">
                     <h3>
